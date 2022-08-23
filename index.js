92380772df547e2c287fe2bd88f3b4013ee80647
Loïc Redon <56895970+loicredon@users.noreply.github.com>
2022-08-23 13:20:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const button = document.getElementById("click-button")
+const schema = document.getElementById("color-scheme")
+const color = document.getElementById("color-pick")
+const mode = document.getElementById("mode-pick")
+const colEl = document.querySelectorAll(".col")
+const hexEl = document.querySelectorAll(".hex")
+let hex = color.value.slice(1)
+console.log(hex)
+
+color.addEventListener('change', function() {
+    hex = color.value.slice(1)
+});
+
+button.addEventListener("click", function() {
+    getColorScheme(hex,mode.value)
+})
+    
+    function getColorScheme(input, scheme) {
+        fetch(`https://www.thecolorapi.com/scheme?hex=${input}&mode=${scheme}&count=5`)
+        .then(response => response.json())
+        .then(data => {
+            for(let i = 0; i < data.colors.length; i++) {
+            colEl[i].style.background = `${data.colors[i].hex.value}`
+            hexEl[i].textContent = data.colors[i].hex.value
+            }
+        })
+    }
+    
+    getColorScheme('000000', 'monochrome')
+
+
+// <img src=${data.colors[i].image.bare}>
+// <p>${data.colors[i].hex.value}</p>
